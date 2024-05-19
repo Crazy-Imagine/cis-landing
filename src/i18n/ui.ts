f30cc73281330428d67ed0e4bb5d93988ef474a7
David Delgado <delgadoch.david@gmail.com>
2024-05-19 01:48:48 -0400

    create initial components for Icons, create first Header approach

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -5,17 +5,53 @@ export const languages = {
 
 export const defaultLang = 'en';
 
+export const showDefaultLang = false;
+
 export const ui = {
   en: {
+    // Aria Keys
+    'aria.home-link': 'Enlace a la página de inicio',
+    'aria.contact-us-link': 'Enlace a la página de contáctanos',
+    'aria.logo': "Crazy Imagine Software's Logo",
+    'aria.facebook': 'Logo de Facebook',
+    'aria.instagram': 'Logo de Instagram',
+    'aria.linkedin': 'Logo de LinkedIn',
+    'aria.twitter': 'Logo de X',
+    'aria.upwork': 'Logo de Upwork',
+    'aria.youtube': 'Logo de Youtube',
+
+    // Common Keys
     'common.hello': 'Hello',
-    'nav.home': 'Home',
+    'common.contact-us': 'Contact Us',
+
+    // Nav Keys
     'nav.about': 'About',
-    'nav.twitter': 'Twitter',
+    'nav.projects': 'Projects',
+    'nav.services': 'Services',
+    'nav.blog': 'Blog',
+    'nav.work-with-us': 'Work with us',
   },
   es: {
+    // Aria keys
+    'aria.home-link': 'Link to home page',
+    'aria.contact-us-link': 'Link to contact us page',
+    'aria.logo': 'Logo de Crazy Imagine Software',
+    'aria.facebook': "Facebook's Logo",
+    'aria.instagram': "Instagram's Logo",
+    'aria.linkedin': "LinkedIn's Logo",
+    'aria.twitter': "X's Logo",
+    'aria.upwork': "Upwork's Logo",
+    'aria.youtube': "Youtube's Logo",
+
+    // Common Keys
     'common.hello': 'Hola',
-    'nav.home': 'Inicio',
+    'common.contact-us': 'Contáctanos',
+
+    // Nav Keys
     'nav.about': 'Sobre nosotros',
-    'nav.twitter': 'Twitter',
+    'nav.projects': 'Proyectos',
+    'nav.services': 'Servicios',
+    'nav.blog': 'Blog',
+    'nav.work-with-us': 'Trabaja con nosotros',
   },
 } as const;
