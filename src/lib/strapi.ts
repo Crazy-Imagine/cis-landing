11c59378e2444f64f98866a764428eef3a76bd04
David Delgado <delgadoch.david@gmail.com>
2024-05-26 16:07:24 -0400

    feat: create Projects section, read data from strapi, create types, update ArticleCard

--- src/lib/strapi.ts
+++ src/lib/strapi.ts
@@ -1,8 +1,23 @@
 import qs from 'qs';
 
+interface Pagination {
+  start?: number;
+  limit?: number;
+  withCount?: boolean;
+}
+
+interface StrapiParams {
+  fields?: string[];
+  filters?: Record<string, any>;
+  locale?: string;
+  pagination?: Pagination;
+  populate?: Record<string, any>;
+  sort?: Record<string, string>;
+}
+
 interface Props {
   endpoint: string;
-  params: any;
+  params: StrapiParams;
 }
 
 export default async function fetchApi<T>({ endpoint, params }: Props) {
