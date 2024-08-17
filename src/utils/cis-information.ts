7d8b810fdc4b784ff5d7dcfa1dcff2739adfb0a8
David Delgado <delgadoch.david@gmail.com>
2024-08-17 15:05:53 -0400

    feat: change phoneNumber

--- src/utils/cis-information.ts
+++ src/utils/cis-information.ts
@@ -1,6 +1,6 @@
 export const CIS_INFORMATION = {
   location: '1786 Smarts Rule St. Kissimmee Florida 34744',
   contactEmail: 'support@crazyimagine.com',
-  phoneNumber: '+1 (320) 322-9488',
-  plainPhoneNumber: '+13203229488',
+  phoneNumber: '+1 (407) 436-4888',
+  plainPhoneNumber: '+14074364888',
 };
