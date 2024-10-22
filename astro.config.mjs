b9472af87ba27c62ea1c32a03a04cb47fc6c05b8
David Delgado <delgadoch.david@gmail.com>
2024-10-22 19:17:08 -0400

    build(deps): add node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
-
 import react from '@astrojs/react';
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,32 +12,32 @@ export default defineConfig({
     defaultLocale: 'en',
     locales: ['en', 'es'],
     routing: {
-      prefixDefaultLocale: false,
-    },
+      prefixDefaultLocale: false
+    }
   },
   image: {
-    domains: ['blogadmin.s3.amazonaws.com', 'blogadmin.s3.us-east-1.amazonaws.com'],
+    domains: ['blogadmin.s3.amazonaws.com', 'blogadmin.s3.us-east-1.amazonaws.com']
   },
   build: {
-    inlineStylesheets: 'always',
+    inlineStylesheets: 'always'
   },
   compressHTML: true,
-  integrations: [
-    tailwind(),
-    react(),
-    sitemap({
-      i18n: {
-        defaultLocale: 'en',
-        locales: {
-          en: 'en-US',
-          es: 'es',
-        },
-      },
-    }),
-  ],
+  integrations: [tailwind(), react(), sitemap({
+    i18n: {
+      defaultLocale: 'en',
+      locales: {
+        en: 'en-US',
+        es: 'es'
+      }
+    }
+  })],
   vite: {
     build: {
-      cssMinify: 'lightningcss',
-    },
+      cssMinify: 'lightningcss'
+    }
   },
+  output: "hybrid",
+  adapter: node({
+    mode: "standalone",
+  })
 });
