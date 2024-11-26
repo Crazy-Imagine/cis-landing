43a785a441474da025a8bf695a3967ef9ea28fcc
Luis Parra <zambranoluis383@gmail.com>
2024-11-26 16:30:13 -0400

    feat(landing): add new landing page

---
+++ src/types/OurServicesLanding.ts
@@ -0,0 +1,13 @@
+export interface Services {
+  title: string;
+  image: string;
+  content: string;
+  id: number;
+}
+
+export interface OurProcessProps {
+  id: number;
+  title: string;
+  image: string;
+  content: string;
+}
