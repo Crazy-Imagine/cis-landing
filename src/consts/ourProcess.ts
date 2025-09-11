8d384a70ab8178b965c157369060b8b822f3af3a
Luis Zambrano <zambranoluis383@gmail.com>
2025-09-11 11:27:58 -0400

    changes in dashboard and create new components

---
+++ src/consts/ourProcess.ts
@@ -0,0 +1,44 @@
+export const OUR_PROCESS_STAFF_AUGMENTATION = [
+  {
+    id: 1,
+    title: 'Entrevista',
+    image: '/src/assets/landing/Group1.webp',
+  },
+  {
+    id: 2,
+    image: '/src/assets/landing/stepsArrow.webp',
+    class: 'h-auto max-h-full rotate-90 lg:rotate-0',
+    containerClass: 'flex h-auto items-center justify-center',
+  },
+  {
+    id: 2,
+    title: 'Selección',
+    image: '/src/assets/landing/Group2.webp',
+    content: 'Selección del talento.',
+  },
+
+  {
+    id: 3,
+    image: '/src/assets/landing/stepsArrow.webp',
+    class: 'h-auto max-h-full rotate-90',
+    containerClass: 'flex h-auto items-center justify-center lg:col-start-3 lg:row-start-2',
+  },
+  {
+    id: 4,
+    title: 'Seguimiento',
+    image: '/src/assets/landing/Group3.webp',
+  },
+
+  {
+    id: 5,
+    image: '/src/assets/landing/stepsArrow.webp',
+    class: 'h-auto max-h-full rotate-90 lg:rotate-180',
+    containerClass: 'flex h-auto items-center justify-center lg:col-start-2 lg:row-start-3',
+  },
+  {
+    id: 4,
+    title: 'Onboarding',
+    image: '/src/assets/landing/Group4.webp',
+    content: 'Formación del talento.',
+  },
+];
