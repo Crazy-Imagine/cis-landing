9b069a5181af96f8d44c57e7f2f6dbdda0e99323
Luis Parra <zambranoluis383@gmail.com>
2026-07-14 09:44:19 -0400

    add new blogs design

--- src/types/Background.ts
+++ src/types/Background.ts
@@ -1,1 +1,1 @@
-export type Background = 'blue' | 'bulbs' | 'white' | 'blueNew' | 'contactUs' | 'blog';
+export type Background = 'blue' | 'bulbs' | 'white' | 'blueNew' | 'contactUs' | 'blog' | 'gray';
